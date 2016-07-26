3abcfb2160cf9ed72146d4e6905f856724775285
almog6564 <almog6564@172.16.59.71>
2016-07-26 16:59:24 +0300

    fixed error in SPPoint, added sortByAxis and its components

--- KDArray.c
+++ KDArray.c
@@ -1,59 +1,136 @@
 #include "KDArray.h"
 #include "SPConfig.h"
 #include "SPLogger.h"
+#include "SPPoint.h"
+#include "math.h"
+
+#define arrayMallocFail(array,j)	 if(!array[j]){ \
+					for(j--;j>=0;j--){ \
+					free(array[j]); \
+					}\
+					free(array); \
+					return NULL
+//TODO add logger message
+
+#define EPS 0.00000000001
 
 /*** Struct Definition ***/
 struct kd_array_t {
-	
+
 	SPPoint* pointArray;
 	int** mat;
-	
+
 	int rows;
 	int cols;
 };
 /****************************/
 
-SPKDArray init (SPConfig attr, SPPoint *arr, int size) {
-    int i;
+struct pointAxis_t {
+	SPPoint* pnt;
+	int axis;
+	int indexInPntArray;
+};
+typedef struct pointAxis_t* PointAxis;
 
-    if(!arr || size<1)
-	//TODO add logger message
-        return NULL;
+SPPoint* copyPointArray(SPPoint* arr, int size) {
+	SPPoint* ret = (SPPoint*) malloc(sizeof(SPPoint) * size);
+	if (!ret) {
+		return NULL;
+	}
+	for (int i = 0; i < size; i++) {
+		ret[i] = spPointCopy(arr[i]);
 
-    /*create new struct SPKDArray*/
-    SPKDArray kd = (SPKDArray)malloc(sizeof(*kd));
-    if(!kd) {
-	//TODO add logger message
-        return NULL;
-    } 
+		/* frees in case of inner malloc fail */
+		arrayMallocFail(ret,i);
+	}
+		return ret;
+	}
+}
 
-    /*copy array into struct*/
-    kd->pointArray = (SPPoint*)malloc(sizeof(SPPoint) * size);
-    if(!kd->pointArray){
-	//TODO add logger message
-	return NULL;
-    }
-    for(i=0; i<size; i++) {
-	kd->pointArray[i] = spPointCopy(arr[i]);
-        if(!kd->pointArray[i]) {
-	    //TODO add logger message
-            goto Cleanup;
-        } 
-    }
+int pointComparator(const void *p, const void *q) {
+	PointAxis p1 = *(const PointAxis *) p;
+	PointAxis p2 = *(const PointAxis *) q;
 
-    /*create initialized matrix of size dims X size */
-    int** M;
-    int dims = spConfigGetPCADim(attr, msg);
-    int* row;
-    for(i=0; i<dims; i++) {
-        row = (int*)malloc(size * sizeof(int));
-	
-    } 
-    
-    /*
-Cleanup:
-    //TODO to be implemented
+	double p1ax = spPointGetAxisCoor(p1->pnt, p1->axis);
+	double p2ax = spPointGetAxisCoor(p2->pnt, p2->axis);
 
-
+	if (fabs(p1ax - p2ax) < EPS)
+		return 0;
+	if (p1ax - p2ax < 0)
+		return -1;
+	return 1;
 
 }
+
+/* assumes all pts same dimension */
+int* sortByAxis(const SPPoint* pts, int size, int axis, const SPConfig config,
+		SPLogger logger, SP_LOGGER_MSG* msg) {
+	if (!pts || axis < 0 || !config || !logger) {
+		spLoggerPrintError("Invalid Argument to sortByAxis", __FILE__, __func__,
+		__LINE__);
+		return NULL;
+	}
+	PointAxis* ptsAx = (PointAxis*) malloc(size * sizeof(*ptsAx));
+
+	if (!ptsAx)
+		//TODO add logger message
+		return NULL;
+
+	for (int i = 0; i < size; i++) {
+		ptsAx[i] = (PointAxis) malloc(sizeof(*ptsAx[i]));
+
+		/*Malloc fail */
+		arrayMallocFail(ptsAx,i);
+
+		ptsAx[i]->pnt = pts[i];
+		ptsAx[i]->axis = axis;
+		ptsAx[i]->indexInPntArray = i;
+
+		}
+
+	}
+	qsort(ptsAx, size, sizeof(*ptsAx), pointComparator);
+	int* ret = (int*) malloc(size * sizeof(int));
+	if (!ret)
+		//TODO add logger message
+		return NULL;
+
+	for (int i = 0; i < size; i++)
+		ret[i] = ptsAx[i]->indexInPntArray;
+
+	return ret;
+}
+
+SPKDArray init(SPConfig attr, SPPoint *arr, int size) {
+	int i;
+
+	if (!arr || size < 1)
+		//TODO add logger message
+		return NULL;
+
+	/*create new struct SPKDArray*/
+	SPKDArray kd = (SPKDArray) malloc(sizeof(*kd));
+	if (!kd) {
+		//TODO add logger message
+		return NULL;
+	}
+
+	/*copy array into struct*/
+	kd->pointArray = copyPointArray(arr, size);
+
+	/*create initialized matrix of size dims X size */
+	int** M;
+	SP_CONFIG_MSG msg;
+	int dims = spConfigGetPCADim(attr, &msg);
+	int* row;
+	for (i = 0; i < dims; i++) {
+		row = (int*) malloc(size * sizeof(int));
+
+	}
+
+	/*
+	 Cleanup:
+	 //TODO to be implemented
+	 */
+}
+
