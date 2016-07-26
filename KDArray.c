8596eac9c70e27f81c844195d5013b18143156bd
Nitzan Carmi <carimnrg@gmail.com>
2016-07-26 14:54:14 +0300

    Partial Skeleton for KDArray.c
    Signed-off-by: Nitzan Carmi <carimnrg@gmail.com>

--- KDArray.c
+++ KDArray.c
@@ -1,4 +1,6 @@
 #include "KDArray.h"
+#include "SPConfig.h"
+#include "SPLogger.h"
 
 /*** Struct Definition ***/
 struct kd_array_t {
@@ -10,3 +12,48 @@ struct kd_array_t {
 	int cols;
 };
 /****************************/
+
+SPKDArray init (SPConfig attr, SPPoint *arr, int size) {
+    int i;
+
+    if(!arr || size<1)
+	//TODO add logger message
+        return NULL;
+
+    /*create new struct SPKDArray*/
+    SPKDArray kd = (SPKDArray)malloc(sizeof(*kd));
+    if(!kd) {
+	//TODO add logger message
+        return NULL;
+    } 
+
+    /*copy array into struct*/
+    kd->pointArray = (SPPoint*)malloc(sizeof(SPPoint) * size);
+    if(!kd->pointArray){
+	//TODO add logger message
+	return NULL;
+    }
+    for(i=0; i<size; i++) {
+	kd->pointArray[i] = spPointCopy(arr[i]);
+        if(!kd->pointArray[i]) {
+	    //TODO add logger message
+            goto Cleanup;
+        } 
+    }
+
+    /*create initialized matrix of size dims X size */
+    int** M;
+    int dims = spConfigGetPCADim(attr, msg);
+    int* row;
+    for(i=0; i<dims; i++) {
+        row = (int*)malloc(size * sizeof(int));
+	
+    } 
+    
+    /*
+Cleanup:
+    //TODO to be implemented
+
+
+
+}
