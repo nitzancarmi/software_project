014ec0bc90399d44e71c18525032d0e112ac8a81
Nitzan Carmi <carimnrg@gmail.com>
2016-07-26 16:58:03 +0300

    Skeleton - Ver No.3 (after merge with almog)
    Signed-off-by: Nitzan Carmi <carimnrg@gmail.com>

--- KDArray.h
+++ KDArray.h
@@ -2,15 +2,21 @@
 #define KDARRAY_H_
 
 #include "SPPoint.h"
-
+#include "SPConfig.h"
+#include "SPLogger.h"
 
 typedef struct kd_array_t* SPKDArray;
 
 /* Initializes the kd-array with the data given by arr. Complexity: O(d*nlogn) */
-SPKDArray init(SPPoint* arr, int size);
+SPKDArray init(SPConfig attr,
+               SPPoint* arr,
+               int size,
+               SPLogger logger,
+               SP_LOGGER_MSG *log_msg,
+	       SP_CONFIG_MSG *conf_msg);
 
 /* Returns two kd-arrays (kdLeft,kdRight) such that the first /ceil[n/2] are in left, rest in right */
-KDArray* Split(SPKDArray kdarr,init coor);
+int Split(SPKDArray kdarr,int coor);
 
 
 #endif /* KDARRAY_H_ */
