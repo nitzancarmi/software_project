e17e69afcf010f05274eb25c54b6124ee176887c
almog6564 <almog6564@172.16.59.71>
2016-08-08 17:16:59 +0300

    added headers

--- SPExtractor.h
+++ SPExtractor.h
@@ -8,9 +8,25 @@
 #ifndef SPEXTRACTOR_H_
 #define SPEXTRACTOR_H_
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "SPPoint.h"
+#include "SPConfig.h"
 
+/*
+ * exports image properties to a unique file
+ * @param pa = SPPoint array contains the image's features
+ * @param size = size of pa
+ * @param image_index = the index of the image in its directory
+ * @param config = configuration attributes
+ *
+ * @return = 0 for success, 1 for failure
+ */
+int exportImageToFile(SPPoint* pa, int size, int image_index, SPConfig config);
 
-
-
+//TODO DOCO
+SPPoint* extractImagesFeatures(int* totalNumOfFeaturesPtr, SPConfig config,
+		SP_LOGGER_MSG* log_msg, SP_CONFIG_MSG* conf_msg);
 
 #endif /* SPEXTRACTOR_H_ */
