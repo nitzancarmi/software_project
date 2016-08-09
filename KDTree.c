e3d6d26da3dafede804168b521d13183a06563c1
almog6564 <almog6564@gmail.com>
2016-08-09 19:17:14 +0300

    Full DOCO Added (no remarks inside functions)

--- KDTree.c
+++ KDTree.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
 
+/* struct definition as explained in the instructions */
 struct kd_tree_node_t {
 
 	int dim;
@@ -13,40 +15,85 @@ struct kd_tree_node_t {
 	SPPoint data;
 };
 
-bool doubleEquals(double a, double b) {  //TODO
+/**
+ * 	Compares two doubles.
+ *
+ *  @param a   	first double
+ *  @param b	second double
+ *  @return
+ *  true if |a-b| < 0.000001   (number of decimal points stored in .feats file)
+ *  false otherwise
+ */
+bool doubleEquals(double a, double b) {
 	if (fabs(a - b) < EPS)
 		return true;
 	return false;
 }
 
-int randomInRange(int min, int max) {  //TODO
+/**
+ * 	Returns an (assumed by C random implementation) uniformly distributed integer
+ * 	between min and max. Each time the function is called a new seed is generated.
+ * 	Only one call to this function during SPCBIR execution so no checks are needed.
+ *
+ *  @param min	minimum of range
+ *  @param max	maximum of range
+ *  @return
+ *  The random integer generated.
+ */
+int randomInRange(int min, int max) {
 	srand(time(NULL));	// Intializes random number generator
 	int range = max - min + 1;
 	return (rand() % range) + min;
 }
 
+/**
+ * 	Calculate the minimum and maximum values between all the points of a SPPoint Array,
+ * 	of the coordinate coor. The values are stored in the pointers max and min.
+ *
+ *	if ptarr == NULL || min == NULL || max == NULL || coor<0 || numOfPts < 1
+ *	than the value INT_MIN is stored in min and max.
+ *
+ *  @param arr 			SPPoint array relevant
+ *  @param numOfPts 	Number of points in arr
+ *  @param coor			The coordinate
+ *  @param max			Address of the double to be the maximum
+ *  @param min			Address of the double to be the min
+ *
+ */
 void getMinMaxOfCoordinate(SPPoint* ptarr, int numOfPts, int coor, double* max,
 		double* min) {
-	if (!ptarr || numOfPts < 0 || coor < 0 || !max || !min)
+	if (!ptarr || numOfPts < 1 || coor < 0 || !max || !min) {
+		declareLogMsg();
+		InvalidError()
+		*min = INT_MIN;
+		*max = INT_MIN;
 		return;
-	//printf("		coor: %d -- ", coor);
+	}
 	*max = spPointGetAxisCoor(ptarr[0], coor);
-	//printf(" %.0f, ", *max);
 	*min = *max;
 	for (int i = 1; i < numOfPts; i++) {
 		double pnt = spPointGetAxisCoor(ptarr[i], coor);
-		//	printf(" %.0f, ", pnt);
 		if (!doubleEquals(pnt, *max))
 			*max = (pnt > *max) ? pnt : *max;
 		if (!doubleEquals(pnt, *min))
 			*min = (pnt < *min) ? pnt : *min;
 	}
-//	printf("\n");
 }
 
+/**
+ *  Calculates the Max Spread dimension of a KD-Array.
+ *  The max spread dimension is the dimension that has the biggest difference between the maximum
+ *  value and the minimum value of all the points in the KDArray in that dimension.
+ *
+ *  @param arr  The relevant SPKDArray
+ *  @return
+ *  -1 if arr == NULL or arr has invalid properties
+ *  The max spread dimension otherwise
+ */
 int getMaxSpreadDimension(SPKDArray arr) {
 	if (!arr)
 		return -1;
+
 	/*Initializaion*/
 	SPPoint* ptarr = getKDPointArray(arr);
 	if (!ptarr)
@@ -57,14 +104,15 @@ int getMaxSpreadDimension(SPKDArray arr) {
 
 	/*The first coordinate is the first candidate*/
 	getMinMaxOfCoordinate(ptarr, numOfPts, 0, &max, &min);
-	//printf("MaxSpread:\n");
+	if (min == INT_MIN && max == INT_MIN)
+		return -1;
+
 	maxSpread = max - min;
-	//printf("	coor = %d, min = %.0f, max = %.0f, maxSpread = %.0f\n", 0, min,max, maxSpread);
+
 	for (int coor = 1; coor < dims; coor++) {
 		getMinMaxOfCoordinate(ptarr, numOfPts, coor, &max, &min);
 		tmpMaxSpread = max - min;
-		//printf("	coor = %d, min = %.0f, max = %.0f, maxSpread = %.0f\n", coor,
-		//		min, max, tmpMaxSpread);
+
 		if (!doubleEquals(tmpMaxSpread, maxSpread)) {
 			if (tmpMaxSpread > maxSpread) {
 				maxSpread = tmpMaxSpread;
@@ -80,10 +128,23 @@ int getMaxSpreadDimension(SPKDArray arr) {
 	return dim;
 }
 
+/**
+ *  Allocates a new SPKDTreeNode based on the arguments given to this function.
+ *
+ *  @param dim  	Dimension the node is split with
+ *  @param val  	Value of the median in the dimension
+ *  @param left		Address of the left SPKDTreeNode child
+ *  @param right	Address of the left SPKDTreeNode child
+ *  @param data		SPPoint array
+ *  @return
+ *  NULL if Memory Allocation failure occurred
+ *  otherwise the allocated node.
+ */
 SPKDTreeNode nodeAllocation(int dim, int val, SPKDTreeNode* left,
-		SPKDTreeNode* right, SPPoint* data, SP_LOGGER_MSG* log_msg) {
+		SPKDTreeNode* right, SPPoint* data) {
 	SPKDTreeNode node = (SPKDTreeNode) malloc(sizeof(*node));
 	if (!node) {
+		declareLogMsg();
 		MallocError()
 		return NULL;
 	}
@@ -95,12 +156,39 @@ SPKDTreeNode nodeAllocation(int dim, int val, SPKDTreeNode* left,
 	return node;
 }
 
+/**
+ *  Actual implementation of the recursive creation of a KDTree based on KDArray.
+ *  was created as a separate function for comfortability to get splitIncrementalDim.
+ *  @see spKDTreeCreate in header.
+ *  Prints the values of the splitted arrays if logger level is Debug.
+ *
+ *  @param KDArray   				The KD-Array from which the KD-Tree will be constructed.
+ *  @param config					Configuration object that contains information about the function execution
+ *  								(e.g. splitMethod)
+ *  @param conf_msg					Pointer to config_msg that can be accessed after execution to know if an error
+ *  								has occurred when config was accessed.
+ *  @param log_msg					Pointer to log_msg that can be accessed after execution to know if an error
+ *  								has occurred when SPLogger was used.
+ *	@param splitIncrementalDim		Current incremental dimension
+ *  @return
+ *  A new SPKDTreeNode in case of a success.
+ *  NULL in case one of the following occurred:
+ *  	- kdarray == NULL || config == NULL || conf_msg == NULL || log_msg == NULL
+ *  	- Memory allocation failure
+ *  	- SPConfig access error
+ */
 SPKDTreeNode spKDTreeCreateRecursion(SPKDArray kdarray, SPConfig config,
 		SP_CONFIG_MSG* conf_msg, SP_LOGGER_MSG* log_msg,
 		int splitIncrementalDim) {
-	//printKDArrayMatrix(kdarray);
-	//printKDPointArray(kdarray);
-	//printf("cols = %d\n", getKDCols(kdarray));
+
+	SP_LOGGER_LEVEL lvl;
+	int printChk = (getLoggerLevel(&lvl) == SP_LOGGER_SUCCESS
+			&& lvl == SP_LOGGER_DEBUG_INFO_WARNING_ERROR_LEVEL);
+	if (printChk) {
+		printKDArrayMatrix(kdarray);
+		printKDPointArray(kdarray);
+		printf("cols = %d\n", getKDCols(kdarray));
+	}
 	if (!kdarray || !config || !conf_msg || !log_msg
 			|| splitIncrementalDim < 0) {
 		InvalidError()
@@ -108,20 +196,17 @@ SPKDTreeNode spKDTreeCreateRecursion(SPKDArray kdarray, SPConfig config,
 	}
 	SPKDTreeNode nodeLeft = NULL, nodeRight = NULL;
 	SPKDArray KDpntr1 = NULL, KDpntr2 = NULL;
-	if (getKDCols(kdarray) == 1) { /*getKDCols(kdarray) == 1*/
-		//	printf("inside\n");
-		//return NULL;
+	if (getKDCols(kdarray) == 1) {
 		SPPoint pnt = getKDOnlyPoint(kdarray);
-		return nodeAllocation(-1, -1, NULL, NULL, &pnt, log_msg);
+		return nodeAllocation(-1, -1, NULL, NULL, &pnt);
 	}
 
 	returnIfConfigMsg(NULL)
-//	printf("**confmsg1 = %d\n",*conf_msg);
 	splitMethod method = spConfigGetSplitMethod(config, conf_msg);
 	returnIfConfigMsg(NULL)
-//	printf("**confmsg2 = %d\n",*conf_msg);
 	int dim, totalDims = spConfigGetPCADim(config, conf_msg);
 	returnIfConfigMsg(NULL)
+
 	/* Chooseing split dimension based on config */
 	switch (method) {
 	case MAX_SPREAD:
@@ -136,41 +221,41 @@ SPKDTreeNode spKDTreeCreateRecursion(SPKDArray kdarray, SPConfig config,
 		dim = randomInRange(0, totalDims);
 		break;
 	case INCREMENTAL:
-//		printf("incremental\n");
 
 		dim = splitIncrementalDim % totalDims;
-//		if(getKDCols(kdarray) <= 3){
-//			printf("dim = %d\n",dim);
-//			printf("IM HERE\n");
-//			return NULL;
-//		}
 		break;
-	default:
+	default:			//method = UNDEFINED - not supposed to get here
 		InvalidError()
 		return NULL;
 	}
-	int split;
+
 	/* Split */
+	int split;
 	if ((split = spKDArraySplit(kdarray, dim, &KDpntr1, &KDpntr2, log_msg,
 			conf_msg)) == -1) {
 		return NULL;
 
 	}
 
-//	printf("**confmsg3 = %d\n",*conf_msg);
 	/* Left Recursion */
-	//printf("\n******LEFT:*******\n");
+	if (printChk)
+		printf("\n******LEFT:*******\n");
+
 	nodeLeft = spKDTreeCreateRecursion(KDpntr1, config, conf_msg, log_msg,
 			splitIncrementalDim + 1);
 	SPKDArrayDestroy(KDpntr1);
 	returnIfConfigMsg(NULL)
+
+
 	/* Right Recursion */
-	//printf("\n******RIGHT:*******\n");
+	if (printChk)
+		printf("\n******RIGHT:*******\n");
+
 	nodeRight = spKDTreeCreateRecursion(KDpntr2, config, conf_msg, log_msg,
 			splitIncrementalDim + 1);
 	SPKDArrayDestroy(KDpntr2);
 	returnIfConfigMsg(NULL)
-	return nodeAllocation(dim, split, &nodeLeft, &nodeRight, NULL, log_msg);
+	return nodeAllocation(dim, split, &nodeLeft, &nodeRight, NULL);
 }
 
 SPKDTreeNode spKDTreeCreate(SPKDArray kdarray, SPConfig config,
