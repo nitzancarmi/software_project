4fd856a60d025b994941c1a89645319bf176ee22
almog6564 <almog6564@gmail.com>
2016-08-09 13:21:09 +0300

    Moved macros to macro.h, removed config arg from split

--- KDArray.h
+++ KDArray.h
@@ -6,24 +6,7 @@
 #include "SPLogger.h"
 #include <stdio.h>
 
-#define EPS 0.00000000001
-#define ERR_ARGS	"%s - at file: %s, line: %d, func: %s"
-#define INVALID 	"Invalid Argument Error"
-#define MALLOC_ERR 	"Memory Allocation Failure"
-#define CONFIG_ERR 	"SPConfig Error"
-#define LOGGER_ERR 	"SPLogger Error"
 
-#define InvalidError()	if((*log_msg = spLoggerPrintError(INVALID,__FILE__,__func__,__LINE__))!= SP_LOGGER_SUCCESS){	\
-							fprintf(stderr,ERR_ARGS,LOGGER_ERR,__FILE__,__LINE__,__func__);}
-
-#define MallocError()	if((*log_msg = spLoggerPrintError(MALLOC_ERR,__FILE__,__func__,__LINE__))!= SP_LOGGER_SUCCESS){	\
-							fprintf(stderr,ERR_ARGS,LOGGER_ERR,__FILE__,__LINE__,__func__);}
-
-#define returnIfConfigMsg(ret) if (*conf_msg != SP_CONFIG_SUCCESS) {	\
-									printf("##CONF_MSG ERR = %d\n",*conf_msg); \
-									InvalidError()\
-									return ret;	\
-								}
 typedef struct kd_array_t* SPKDArray;
 
 /* Initializes the kd-array with the data given by arr. Complexity: O(d*nlogn) */
@@ -32,8 +15,7 @@ SPKDArray spKDArrayCreate(SPConfig attr, SPPoint* arr, int size,
 
 /* Returns two kd-arrays (kdLeft,kdRight) such that the first /ceil[n/2] are in left, rest in right */
 int spKDArraySplit(SPKDArray kdarr, int coor, SPKDArray* KDpntr1,
-		SPKDArray* KDpntr2, SPConfig config, SP_LOGGER_MSG *log_msg,
-		SP_CONFIG_MSG *conf_msg);
+		SPKDArray* KDpntr2, SP_LOGGER_MSG *log_msg, SP_CONFIG_MSG *conf_msg);
 
 /* TODO Documentation
  *
