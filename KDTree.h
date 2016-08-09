1bd8709628bd496289860d026a23cb259ab4b544
almog6564 <almog6564@gmail.com>
2016-08-09 13:20:31 +0300

    Added DOCO

--- KDTree.h
+++ KDTree.h
@@ -3,10 +3,36 @@
 
 #include "KDArray.h"
 
+/** Type for defining the KDTree */
 typedef struct kd_tree_node_t* SPKDTreeNode;
 
-SPKDTreeNode spKDTreeCreate(SPKDArray kdarray,SPConfig config,SP_CONFIG_MSG* conf_msg, SP_LOGGER_MSG* log_msg);
+/**
+ * 	Creating and allocating a new SPKDTreeNode (KD-Tree) recursively from an existing SPKDArray (KD-Array).
+ * 	The splitting of the KD-Array are done using properties defined in the configuration object.
+ * 	The leaves of the tree will contains the points.
+ *
+ *  @param KDArray   	The KD-Array from which the KD-Tree will be constructed.
+ *  @param config		Configuration object that contains information about the function execution
+ *  					(e.g. splitMethod)
+ *  @param conf_msg		Pointer to config_msg that can be accessed after execution to know if an error
+ *  					has occurred when config was accessed.
+ *  @param log_msg		Pointer to log_msg that can be accessed after execution to know if an error
+ *  					has occurred when SPLogger was used.
+ *  @return
+ *  A new SPKDTreeNode in case of a success.
+ *  NULL in case one of the following occurred:
+ *  	- kdarray == NULL || config == NULL || conf_msg == NULL || log_msg == NULL
+ *  	- Memory allocation failure
+ *  	- SPConfig access error
+ */
+SPKDTreeNode spKDTreeCreate(SPKDArray KDArray,const SPConfig config,SP_CONFIG_MSG* conf_msg, SP_LOGGER_MSG* log_msg);
 
-void spKDTreeDestroy(SPKDTreeNode kdtree);
+/**
+ * spKDTreeDestroy: Deallocates an existing KDTreeNode and all its descendants
+ *
+ * @param KDTree Target KDTree to be deallocated. If KDTree is NULL nothing
+ * will be done.
+ */
+void spKDTreeDestroy(SPKDTreeNode KDTree);
 
 #endif /* KDTREE_H_ */
