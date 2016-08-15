9129308299b21f9b0f30f1b1a3e9b50649b3d174
Nitzan Carmi <carimnrg@gmail.com>
2016-08-15 10:41:03 +0300

    added unnecessary files to repository
    Signed-off-by: Nitzan Carmi <carimnrg@gmail.com>

--- SPExtractor.c
+++ SPExtractor.c
@@ -3,9 +3,13 @@
 #include <string.h>
 #include "SPExtractor.h"
 
+#define declareLogMsg() SP_LOGGER_MSG _log_msg = SP_LOGGER_SUCCESS, *log_msg = &_log_msg
+
+
 /******************************************************************************************************/
 /*************************************** Import from Files ********************************************/
 /******************************************************************************************************/
+
 
 /**
  * This function reads the next entire line from file stream, allocates enough space to store the text
@@ -110,10 +114,112 @@ int getIntFromLine(FILE* feats) {
 		}
 	}
 	/* no need for line anymore */
+=======
+int getline(char **lineptr, int* n, FILE *stream) {
+	char *bufptr = NULL;
+	char *p = bufptr;
+	int size;
+	int c;
+
+//	if (!lineptr || !stream || !n) {
+//		return -1;
+//	}
+	bufptr = *lineptr;
+	size = *n;
+
+	c = fgetc(stream);
+	if (c == EOF) {
+		printf("\n\n##1\n\n");
+		return -1;
+	}
+	if (bufptr == NULL) {
+		bufptr = malloc(128);
+		if (bufptr == NULL) {
+			printf("\n\n##2\n\n");
+			return -1;
+		}
+		size = 128;
+	}
+	p = bufptr;
+	while (c != EOF) {
+		if ((p - bufptr) > (size - 1)) {
+			size = size + 128;
+			bufptr = realloc(bufptr, size);
+			if (bufptr == NULL) {
+				printf("\n\n##3\n\n");
+				return -1;
+			}
+		}
+		*p++ = c;
+		if (c == '\n') {
+			break;
+		}
+		c = fgetc(stream);
+	}
+
+	*p++ = '\0';
+	*lineptr = bufptr;
+	*n = size;
+
+	return p - bufptr - 1;
+}
+
+int SPgetLine(char** linePtr, FILE* feats) {
+	declareLogMsg();
+	char line[50000];
+
+	//int chk = getline(&line, sizeBuffer, feats);
+	//int chk = 0;
+	char* chk = fgets(line, 50000, feats);
+	if (!chk) {	//Error checking for getline
+		//free(line);
+		//printf("\nchkline = %p",chk,line,0);
+		printWarning("Error while reading the file. SKIPPING FILE")
+		return -1;
+	}
+	int size = strlen(line);
+
+	line[size] = '\0';
+	*linePtr = (char*) malloc(size+1);
+	if (*linePtr == NULL) {
+		MallocError()
+		return -1;
+	}
+
+	strcpy(*linePtr, line);                        // Copy the characters
+	//*linePtr[size - 1] = '\0';
+	return 0;
+}
+
+int getIntFromLine(FILE* feats) {
+	declareLogMsg();
+	long int imageIndex = -1;
+	char* line = NULL;
+	if (SPgetLine(&line, feats) == -1) {
+		return -1;
+	}
+
+	char* ptrChk;
+
+	//printf("line = %s\n", line);
+	imageIndex = strtol(line, &ptrChk, 0);
+	if (ptrChk != NULL) {
+		char* tmp = ptrChk;
+		while (*tmp != '\0') {
+			if (!isspace(*tmp)) {
+				printWarning("Letters found on file. SKIPPING FILE")
+				free(line);
+				return -1;
+			}
+			tmp++;
+		}
+	}
+>>>>>>> added unnecessary files to repository
 	free(line);
 	return imageIndex;
 }
 
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 /**
  * This function reads an array of doubles from a string, each one can be separated by space characters
  * beforehand and afterhand. The doubles are then parsed by the @strtod function. No other characters
@@ -137,6 +243,9 @@ int getIntFromLine(FILE* feats) {
  */
 double* getDataFromLine(char* line, int dim, int lineNumber) {
 
+=======
+double* getDataFromLine(char* line, int dim,int lineNumber) {
+>>>>>>> added unnecessary files to repository
 	declareLogMsg();
 
 	if (!line || dim <= 0) {
@@ -144,6 +253,7 @@ double* getDataFromLine(char* line, int dim, int lineNumber) {
 		return NULL;
 	}
 
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 	/* Init */
 	char *c = line;
 	char *tail = NULL;
@@ -154,10 +264,14 @@ double* getDataFromLine(char* line, int dim, int lineNumber) {
 
 	/* allocated double array exactly the size of dim (spCADimension) */
 	ret = (double*) calloc(dim, sizeof(double));
+=======
+	double* ret = (double*) malloc(dim * sizeof(double));
+>>>>>>> added unnecessary files to repository
 	if (!ret) {
 		MallocError()
 		return NULL;
 	}
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 
 	/* read entire line */
 	while (lineCnt != lineLength && *c != '\0' && *c != EOF) { //EOF not supposed to happen, sanity check
@@ -194,6 +308,32 @@ double* getDataFromLine(char* line, int dim, int lineNumber) {
 		singleNumber = strtod(buffer, &tail);
 
 		/* Error reading the double */
+=======
+	char* c = line;
+	int count = 0,i;
+
+	while (*c != '\0') {
+		i = 0;
+		char buffer[1024] = { '\0' };
+
+		/** Read one double **/
+		while (*c != ' ' && *c != '\0' && *c!= '\n') {
+			if (!isdigit(*c) && *c != '.' && *c != '-') {
+				char error[1024] = {'\0'};
+				sprintf(error,"%s feature = %d, line = %d, *c = <%d>\nline:\n%s",SYNTAX,count,lineNumber,*c,line);
+				printWarning(error)
+				free(ret);
+				return NULL;
+			}
+			buffer[i++] = *c;
+			c++;
+		}
+		char* tail = NULL;
+		buffer[i] = '\0';
+		double singleNumber = strtod(buffer, &tail);
+
+		/** Error reading the double **/
+>>>>>>> added unnecessary files to repository
 		if (tail != NULL) {
 			if (*tail != '\0') {
 				printWarning(SYNTAX)
@@ -201,26 +341,41 @@ double* getDataFromLine(char* line, int dim, int lineNumber) {
 				return NULL;
 			}
 		}
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 		/* found more numbers than dim - skipping file*/
 		if (count > dim) {
 			warningWithArgs(FEAT_SIZE_ERR, count, dim);
+=======
+		if (count > dim + 1) {
+			printf("count = %d, dim = %d\n", count, dim);
+			printWarning(
+					"Feature size is bigger than spCADimension. SKIPPING FILE")
+>>>>>>> added unnecessary files to repository
 			free(ret);
 			return NULL;
 		}
 
 		ret[count++] = singleNumber;
 		c++;
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 		lineCnt++;
 	}
 
 	/* Essentially found less numbers than dim, same error message  - skipping file*/
 	if (count != dim) {
 		warningWithArgs(FEAT_SIZE_ERR, count, dim);
+=======
+	}
+	if (count != dim) {
+		printWarning(
+				"Feature size is smaller than spCADimension. SKIPPING FILE")
+>>>>>>> added unnecessary files to repository
 		free(ret);
 		return NULL;
 	}
 
 	return ret;
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 }
 
 /*
@@ -250,6 +405,12 @@ double* getDataFromLine(char* line, int dim, int lineNumber) {
  * 		- The file doesn't contain the same amount of features as written on the second line of the file exactly
  * 0 for successful operation
  */
+=======
+
+}
+
+//Not using the index in the path
+>>>>>>> added unnecessary files to repository
 SPPoint* extractSingleImage(int imgIndex, int* numOfFeatures, SPConfig config) {
 	declareLogMsg();
 
@@ -257,6 +418,7 @@ SPPoint* extractSingleImage(int imgIndex, int* numOfFeatures, SPConfig config) {
 		InvalidError()
 		return NULL;
 	}
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 	/*** Initializations ***/
 	char *ptr,
 	*line = NULL, filepath[1024] = { '\0' };
@@ -364,19 +526,117 @@ SPPoint* extractSingleImage(int imgIndex, int* numOfFeatures, SPConfig config) {
 	}
 
 	return pntsArray;
+=======
+
+	/*** Change filepath to .feats ***/
+	char filepath[1024] = {'\0'};
+	SP_CONFIG_MSG _conf_msg = spConfigGetImagePath(filepath, config, imgIndex),
+	*conf_msg = &_conf_msg;
+	returnIfConfigMsg(NULL);
+
+	char* ptr = filepath + strlen(filepath);
+	while (*ptr != '.') {
+		*ptr = '\0';
+		ptr--;
+	}
+	sprintf(ptr, ".feats");
+	/*********************************/
+
+	FILE* feats = fopen(filepath, "r");
+	if (!feats) {
+		printWarning("File cannot be opened. SKIPPING FILE")
+		return NULL;
+	}
+
+	int imageIndex = getIntFromLine(feats);
+	if (imageIndex == -1) {
+		fclose(feats);
+		return NULL;
+	}
+
+	*numOfFeatures = getIntFromLine(feats);
+	if (*numOfFeatures == -1) {
+		fclose(feats);
+		return NULL;
+	}
+
+	int countOfFeatures = 0;
+	int dim = spConfigGetPCADim(config, conf_msg);
+	SPPoint* ret = (SPPoint*) calloc(*numOfFeatures, sizeof(*ret));	//Returned array
+
+	while (!feof(feats)) {
+
+		char* line = NULL;
+		int lineChk = SPgetLine(&line, feats);
+
+		if (lineChk == -1) {
+			//Error message printed inside
+			fclose(feats);
+			return NULL;
+		}
+
+		double* data = getDataFromLine(line, dim,countOfFeatures+3);
+		if (!data) {
+			printWarning("DATA ERROR")
+			//TODO Delete
+			//Error message printed inside
+			fclose(feats);
+			return NULL;
+		}
+
+		SPPoint currentPoint = spPointCreate(data, dim, imageIndex);
+
+		/*** ERRORS ***/
+		if (!currentPoint || countOfFeatures > *numOfFeatures ) {
+			if (!currentPoint) {
+				MallocError()
+				free(data);
+			} else {
+				spPointDestroy(currentPoint);
+				char error[1024] = {'\0'};
+				sprintf(error,"Features file %d features, while %d was the quantity defined. SKIPPING FILE",countOfFeatures,*numOfFeatures);
+				printWarning(error)
+			}
+			spPointArrayDestroy(ret, *numOfFeatures);
+
+			fclose(feats);
+			return NULL;
+		}
+		/************/
+
+		ret[countOfFeatures++] = currentPoint;
+
+	}
+
+	if (countOfFeatures != *numOfFeatures) {
+		printWarning(
+				"Features file contains less features than defined in it. SKIPPING FILE")
+		spPointArrayDestroy(ret, *numOfFeatures);
+		free(ret);
+		fclose(feats);
+		return NULL;
+	}
+
+	//return NULL;
+	return ret;
+>>>>>>> added unnecessary files to repository
 }
 
 SPPoint* extractImagesFeatures(int* totalNumOfFeaturesPtr, SPConfig config,
 		SP_LOGGER_MSG* log_msg, SP_CONFIG_MSG* conf_msg) {
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 	SPPoint* singleImageFeatures = NULL;
 	int* featAddr;
 
+=======
+>>>>>>> added unnecessary files to repository
 	if (!config || !log_msg || !conf_msg)
 		return NULL;
 
 	int numOfImages = spConfigGetNumOfImages(config, conf_msg);
 
 	/** First maintain a SPPoint matrix -
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 	 * each row is for a single image (every SPPoint is a feature of the said image)
 	 * Also maintain a int array for the number of features for each image,
 	 * because the number of features may vary.
@@ -389,12 +649,23 @@ SPPoint* extractImagesFeatures(int* totalNumOfFeaturesPtr, SPConfig config,
 	SPPoint** imagesFeatures = (SPPoint**) calloc(numOfImages,	//matrix to hold
 			sizeof(SPPoint*));
 
+=======
+	 * each line is for a single image (every SPPoint is a feature of the said image)
+	 * Also maintain a int array for the number of features for each image,
+	 * because the number of features may vary.
+	 */
+	int* numOfFeatures = (int*) calloc(numOfImages, sizeof(int));
+
+	SPPoint** imagesFeatures = (SPPoint**) calloc(numOfImages,
+			sizeof(SPPoint*));
+>>>>>>> added unnecessary files to repository
 	if (!numOfFeatures || !imagesFeatures) {
 		MallocError()
 		free(numOfFeatures);
 		free(imagesFeatures);
 		return NULL;
 	}
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 
 	int imgCount = 0;
 	for (int img = 0; img < numOfImages; img++) {
@@ -428,22 +699,52 @@ SPPoint* extractImagesFeatures(int* totalNumOfFeaturesPtr, SPConfig config,
 	}
 
 	/** Overall number of features added **/
+=======
+	int imgCount = 0;
+	for (int img = 0; img < numOfImages; img++) {
+
+		/* Extract feautres of single image from the appropriate file*/
+		SPPoint* singleImageFeatures = extractSingleImage(img,
+				&numOfFeatures[img], config);
+		if (!singleImageFeatures) {
+			printf("error %d, ", img);
+			continue;
+		} else
+			imagesFeatures[imgCount++] = singleImageFeatures;
+
+	}
+	printf("\n");
+	if (imgCount < numOfImages)
+		printWarning("Some of the images were not imported.");
+
+>>>>>>> added unnecessary files to repository
 	int totalNumOfFeatures = 0;
 	for (int i = 0; i < numOfImages; i++) {
 		totalNumOfFeatures += numOfFeatures[i];
 	}
 	*totalNumOfFeaturesPtr = totalNumOfFeatures;
 
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 	/** Copy all points from the matrix to a large single point
 	 *  array, to be inserted to a kdarray **/
 	SPPoint* allFeatures = (SPPoint*) calloc(totalNumOfFeatures,
 			sizeof(*allFeatures));
 
+=======
+	/* Copy all points from the matrix to a single big point array, to be inserted to a kdarray */
+
+	SPPoint* allFeatures = (SPPoint*) calloc(totalNumOfFeatures,
+			sizeof(*allFeatures));
+>>>>>>> added unnecessary files to repository
 	/** allocation error **/
 	if (!allFeatures) {
 		MallocError()
 
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 		for (int img = imgCount - 1; img >= 0; img--) {
+=======
+		for (int img = imgCount; img >= 0; img--) {
+>>>>>>> added unnecessary files to repository
 			spPointArrayDestroy(imagesFeatures[img], numOfFeatures[img]);
 		}
 		free(numOfFeatures);
@@ -457,6 +758,7 @@ SPPoint* extractImagesFeatures(int* totalNumOfFeaturesPtr, SPConfig config,
 
 		int features = numOfFeatures[img];
 		for (int feat = 0; feat < features; feat++) {
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 			//insertion to returned array
 			allFeatures[++currentFeature] = imagesFeatures[img][feat];
 		}
@@ -466,6 +768,12 @@ SPPoint* extractImagesFeatures(int* totalNumOfFeaturesPtr, SPConfig config,
 	free(numOfFeatures);
 	free(imagesFeatures); //only pointer to matrix need to be freed - features themselves still being used later
 
+=======
+			allFeatures[++currentFeature] = imagesFeatures[img][feat];
+		}
+	}
+
+>>>>>>> added unnecessary files to repository
 	return allFeatures;
 }
 
@@ -473,6 +781,7 @@ SPPoint* extractImagesFeatures(int* totalNumOfFeaturesPtr, SPConfig config,
 /**************************************** Export to Files *********************************************/
 /******************************************************************************************************/
 
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 int exportImageToFile(SPPoint* pointArray, int size, int image_index,
 		SPConfig config) {
 	declareLogMsg();
@@ -542,18 +851,79 @@ int exportImageToFile(SPPoint* pointArray, int size, int image_index,
 					/** Regular non-at-some-kind-of-end feature **/
 					endChar = ' ';
 				}
+=======
+int exportImageToFile(SPPoint* pa, int size, int image_index, SPConfig config) {
+	declareLogMsg();
+	int rc = 0;
+	char filepath[1024] = {'\0'};
+	SP_CONFIG_MSG conf_msg = spConfigGetImagePath(filepath, config,
+			image_index);
+	if (conf_msg != SP_CONFIG_SUCCESS)
+	return 1;
+
+	/*** Change filepath to .feats ***/
+	char* ptr = filepath + strlen(filepath);
+	while (*ptr != '.') {
+		*ptr = '\0';
+		ptr--;
+	}
+	sprintf(ptr, ".feats");
+	printf("%s\n", filepath); //TODO delete
+			/*********************************/
+
+			FILE *output = fopen(filepath, "w");
+			if (!output) {
+				printError("Cannot create feats file")
+				return 1;
+			}
+
+			/*writes image index and num of points at the beginning of file*/
+			rc = fprintf(output, "%d\n%d\n", image_index, size);
+			if (rc < 1) {
+				printError("Print to feats file error")
+				fclose(output);
+				return 1;
+			}
+
+			/*writes double array inside points*/
+			int i,
+	j, dims;
+	SPPoint curr;
+	for (i = 0; i < size; i++) {
+		curr = pa[i];
+		dims = spPointGetDimension(curr);
+		for (j = 0; j < dims; j++) {
+
+			char endChar;
+			if ((j + 1) == dims && i == size - 1) {
+				rc = fprintf(output, "%f", spPointGetAxisCoor(curr, j));
+			} else {
+				if ((j + 1) == dims && i != size - 1) {
+					endChar = '\n';
+				} else
+					endChar = ' ';
+>>>>>>> added unnecessary files to repository
 				rc = fprintf(output, "%f%c", spPointGetAxisCoor(curr, j),
 						endChar);
 			}
 
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 			/** file write error **/
 			if (rc < 1) {
 				printError(FEATS_PRNT)
+=======
+			if (rc < 1) {
+				//TODO logger message
+>>>>>>> added unnecessary files to repository
 				fclose(output);
 				return 1;
 			}
 		}
 	}
 	fclose(output);
+<<<<<<< c4c617ed7625e57c6764432743f8adacd84836d6
 	return OK;
+=======
+	return 0;
+>>>>>>> added unnecessary files to repository
 }
